8087a2b354fb348183c88d2732227e13c88f8b59
bmartin86 <bolanca.martin@gmail.com>
2017-12-04 17:35:11 +0100

    zadaća 04.12.

--- mbolanca/zadaca.04-12-2017/zadaci-6.7.php
+++ mbolanca/zadaca.04-12-2017/zadaci-6.7.php
@@ -1,8 +1,52 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+echo "<h1>1. zadatak</h1>";
+echo 'Napišite program koji upisuje pet imena u polje $imena i ispisuje ih '
+. 's pomoću petlje "foreach".<br><br>';
+
+$imena=array("Brian","Keith","Mick","Charlie","Ron");
+
+foreach($imena as $ime)
+{
+    echo $ime."<br>";
+}
+
+echo "<hr>";
+
+echo "<h1>2. zadatak</h1>";
+echo 'Polju iz prethodnog zadatka promijenite ključeve tako da umjesto '
+. 'ključa "1" piše "ime5", umjesto ključa "2" "ime4" itd.<br><br>';
+
+$imena=array();
+
+$imena["ime5"]= "Brian";
+$imena["ime4"]= "Keith";
+$imena["ime3"]= "Mick";
+$imena["ime2"]= "Charlie";
+$imena["ime1"]= "Ron";
+
+foreach($imena as $key=>$ime)
+{
+    echo $key." = ".$ime."<br>";
+}
+
+echo "<hr>";
+
+echo "<h1>3. zadatak</h1>";
+echo 'Posložite imena u polju iz prvog zadatka abecednim redom '
+. 'i ispišite vrijednosti elemenata uz pomoć petlje "for".<br><br>';
+
+$imena=array("Brian","Keith","Mick","Charlie","Ron");
+
+sort($imena);
+
+for($i=0;$i<count($imena);$i++)
+{
+    echo $imena[$i]."<br>";
+}
+
+echo "<hr>";
+
+
+
 
