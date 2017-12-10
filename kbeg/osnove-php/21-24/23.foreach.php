66ea693ec8df9fb4a069391d04c57c1e272f8434
kristinabeg <kristina.beg@gmail.com>
2017-12-10 11:59:00 +0100

    izmjene

---
+++ kbeg/osnove-php/21-24/23.foreach.php
@@ -0,0 +1,10 @@
+<?php
+/**
+Prikazati rad s foreach petljom. Obraditi rad s poljima.
+Prikazati rad samo s vrijednostima i rad s ključevima i
+vrijednostima. Objasniti ključne riječi break i continue,
+prikazati gdje i kako se koriste.
+*/
+
+
+?>
