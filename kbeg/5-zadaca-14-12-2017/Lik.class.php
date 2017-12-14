1d3c7da78b8830da249f29f26f0785977347e9ee
kristinabeg <kristina.beg@gmail.com>
2017-12-14 14:15:14 +0100

    jos dio zadace

--- kbeg/5-zadaca-14-12-2017/Lik.class.php
+++ kbeg/5-zadaca-14-12-2017/Lik.class.php
@@ -18,7 +18,7 @@ abstract class Lik {
     public abstract function povrsina();
     public function toString() {        
         //echo "\nJa sam objekt ".get_class($this).", a nastao sam iz klase ".CLASS;
-        echo "\nJa sam objekt ".get_class($this).", a nastao sam iz klase ";
+        echo "\nJa sam objekt ".get_class($this).", a nastao sam iz klase Lik<br>";     //editirano
     }
 }
 
@@ -36,22 +36,21 @@ interface IKvadrat {
 //Class Kvadrat contains 1 abstract method and must therefore be declared abstract or implement the 
 //remaining methods (IKvadrat::brojstranica)
 class Kvadrat extends Lik implements IKvadrat {
-    private $a; // stranica kvadrata
+    public $a; // stranica kvadrata     // <-- public umjesto private
     function __construct($a) {
+        $this->a = $a;                      // <-- dodano
     }
-
+    
     public function opseg() {
         // vrati opseg
         // return 
-        $opseg=4*$a;                    // <-- dodano
-        return $opseg;                  // <-- dodano
+        return 4 * $this->a;                // <-- dodano
     }
 
     public function povrsina() {
         // vrati povrsinu
         //return
-        $povrsina=a*a;                  // <-- dodano
-        return $povrsina;               // <-- dodano
+        return $this->a * $this->a;         // <-- dodano
     }
     
     public function brojstranica() {    // <-- dodano
@@ -59,10 +58,11 @@ class Kvadrat extends Lik implements IKvadrat {
     }
 }
 
-class Krug extends Lik {
+class Krug extends Lik implements ILik{
     private $r; // polumjer kruga
+    private $boja='crvena';
     function __construct($r) {
-// postavi svojstvo polumjera
+    // postavi svojstvo polumjera
         $this->r = $r;
     }
 
@@ -72,8 +72,13 @@ class Krug extends Lik {
     }
 
     public function povrsina() {
-        // vrati povrsinu
+        // vrati povrsinu (r*r*pi)
         //return
+        return $this->r * $this->r * 3.14;
+    }
+    
+    public function boja(){
+       return $this->boja; 
     }
 }
 
@@ -81,11 +86,22 @@ class Krug extends Lik {
 $k1 = new Krug(5);
 
 // ispiši opseg
+echo "Opseg kruga iznosi: ";              // <-- dodano
 echo $k1->opseg();
+echo "<br>";
+
+echo "Površina kruga iznosi: ";              // <-- dodano
+echo $k1->povrsina();
+echo "<br>";
 
 // ispiši opis klase
+echo "Opis klase: ";
 $k1->toString();
+echo "<br>";
 
+echo "Boja kruga: ";
+$k1->boja();
+echo "<br>";
 // postavi i ispiši boju kruga
 // ?
 // postavi i ispiši koordinatu središta kruga
@@ -94,10 +110,14 @@ $k1->toString();
 $k2 = new Kvadrat(5);
 
 // ispiši opseg
+echo "Opseg kvadrata: ";
 echo $k2->opseg();
+echo "<br>";
 
 // ispiši opis klase
+echo "Opis klase: ";
 $k2->toString();
+echo "<br>";
 
 // ispiši broj stranica kvadrata
 // ?
