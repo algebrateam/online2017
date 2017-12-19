cb975a621fb7a9fcbd2974b9c1b1fbe1f1631fda
mrvic <mrvic.predrag@gmail.com>
2017-12-19 01:27:22 +0100

    Namespace i autoload

--- pmrvic/osnove-php/37-40/ponavljanje.php
+++ pmrvic/osnove-php/37-40/ponavljanje.php
@@ -1,65 +1,68 @@
 <?php
 
-class A
-{
+class A {
+
     const NAME = 'A';
+
     public static function test() {
         $args = func_get_args();
-        echo static::NAME, " ".join(',', $args)." \n";
+        echo static::NAME, " " . join(',', $args) . " \n";
     }
+
 }
+
 A::test();
-$a1=new A();
+$a1 = new A();
 $a1->test();
 
-class boo
-{
-  public static function something()
-  {
-    echo"<hr>".  __CLASS__." "; // foo
-   // var_dump($this);
-  }
+class boo {
+
+    public static function something() {
+        echo"<hr>" . __CLASS__ . " "; // foo
+        // var_dump($this);
+    }
+
 }
+
 boo::something();
 
-class foo
-{
-  public function something()
-  {
-    echo"<hr>".  __CLASS__." "; // foo
-    var_dump($this);
-  }
+class foo {
+
+    public function something() {
+        echo"<hr>" . __CLASS__ . " "; // foo
+        var_dump($this);
+    }
+
 }
 
-class foo_bar extends foo
-{
-  public function something()
-  {
-    echo"<hr>".  __CLASS__." "; // foo_bar
-    var_dump($this);
-  }
-    public function call()
-  {
-    echo self::something(); // self
-    echo parent::something(); // parent
-    echo boo::something(); // grandparent any class..
-  }
+class foo_bar extends foo {
+
+    public function something() {
+        echo"<hr>" . __CLASS__ . " "; // foo_bar
+        var_dump($this);
+    }
+
+    public function call() {
+        echo self::something(); // self
+        echo parent::something(); // parent
+        echo boo::something(); // grandparent any class..
+    }
+
 }
 
-class foo_bar_baz extends foo_bar
-{
-  public function something()
-  {
-    echo "<hr>". __CLASS__." "; // foo_bar_baz
-    var_dump($this);
-  }
+class foo_bar_baz extends foo_bar {
 
-  public function call()
-  {
-    echo self::something(); // self
-    echo parent::something(); // parent
-    echo foo::something(); // grandparent
-  }
+    public function something() {
+        echo "<hr>" . __CLASS__ . " "; // foo_bar_baz
+        var_dump($this);
+    }
+
+    public function call() {
+        echo self::something(); // self
+        echo parent::something(); // parent
+        echo foo::something(); // grandparent
+    }
+
 }
 
 ///error_reporting(-1);
