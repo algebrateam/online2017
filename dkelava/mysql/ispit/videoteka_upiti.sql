09f62a05dde2d6f19f352a190b6c84f1bb553e98
danijelkelava <dykyprod@gmail.com>
2018-01-11 15:07:02 +0100

    parcijalni gotov

--- dkelava/mysql/ispit/videoteka_upiti.sql
+++ dkelava/mysql/ispit/videoteka_upiti.sql
@@ -30,38 +30,3 @@ from Clanovi
 inner join Posudba on Clanovi.id=Posudba.Clan_id
 inner join Film on Film.id=Posudba.Film_id
 where Posudba.Datum_vracanja is null;
-
-
--- JOS NEKE NAREDBE
-
--- naredba vrati koliko je filmova clan posudio
-select distinct
-Clanovi.Ime,
-Clanovi.Prezime,
-count(*) as 'Posudio filmova'
-from Clanovi
-inner join Posudba on Clanovi.id=Posudba.Clan_id
-where Datum_vracanja is null
-group by Posudba.Clan_id;
-
--- naredba koja ukljuci i ime zanra
-select
-Clanovi.Ime,
-Clanovi.Prezime,
-Posudba.Datum_posudbe,
-Film.Ime_filma as 'Posudio film',
-Zanr.Ime_zanra as Zanr
-from Clanovi
-inner join Posudba on Clanovi.id=posudba.Clan_id
-inner join Film on Film.id=Posudba.Film_id
-inner join Zanr on Film.Sifra_zanra=Zanr.id
-where Posudba.Datum_vracanja is null;
-
--- provjera koliko je puta neki film trenutno posudjen
-select
-Film.Ime_filma,
-count(*) as'Ukupno posudjen'
-from Posudba
-inner join Film on Posudba.Film_id=Film.id
-group by Posudba.Film_id;
-
