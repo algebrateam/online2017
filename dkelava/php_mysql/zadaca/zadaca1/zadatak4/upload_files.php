b4aa73dd12ac823969555700f19bfa540c5a7c2d
danijelkelava <dykyprod@gmail.com>
2018-01-24 19:34:02 +0100

    upload

--- dkelava/php_mysql/zadaca/zadaca1/zadatak4/upload_files.php
+++ dkelava/php_mysql/zadaca/zadaca1/zadatak4/upload_files.php
@@ -7,8 +7,20 @@ if(!is_numeric($_GET['broj_fileova'])){
 $upload_forma = "";
 $upload_forma .= "<form method='post' enctype='multipart/form-data'>";
 for ($i = 0; $i < $_GET['broj_fileova']; $i++) {
-    $upload_forma .= "<input type='file' name='file'><br/>";
+    $upload_forma .= "<input type='file' name='file[]' multiple><br/>";
 }
+$upload_forma .= "<input type='submit' name='upload' value='upload'>";
 $upload_forma .= "</form>";
 
 echo $upload_forma;
+
+if (isset($_POST['upload'])) {
+    print_r($_POST['upload']);
+    echo "<br/>";
+    foreach ($_FILES['file']['name'] as $key => $value) {
+    echo $_FILES['file']['name'][$key];
+    echo "<br>";
+    move_uploaded_file($_FILES['file']['tmp_name'][$key], "test/" . $_FILES['file']['name'][$key]);
+    }
+}
+
